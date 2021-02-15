51298a35ac0fce3af70df746adf6bcb0dfe11087
wiggocd <cd.wiggins@outlook.com>
2021-02-15 20:40:01 +0000

    Hide window before close to improve UX

--- src/customtitlebar.ts
+++ src/customtitlebar.ts
@@ -1,13 +1,23 @@
 import { Titlebar, Color } from 'custom-electron-titlebar';
+import { remote } from 'electron';
+import $ from 'jquery';
 
 const iconRelativeToRoot = 'resources/icon.png';
 
 createTitlebar();
 
 function createTitlebar() {
-    new Titlebar({
+    const titlebar = new Titlebar({
         backgroundColor: Color.fromHex('#2e2e2e'),
         icon: iconRelativeToRoot,
         overflow: 'hidden'
+    });
+
+    document.title = '';
+    titlebar.updateTitle();
+
+    const closeButton = $('.window-close')[0];
+    closeButton.addEventListener('click', (_event) => {
+        remote.getCurrentWindow().hide();
     });
 }
