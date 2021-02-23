71f7ca3c3e2d919c7f0a506956345af8ac4a36fc
C. Wiggins <cd.wiggins@outlook.com>
2021-02-23 20:01:02 +0000

    Added preliminary outdent support

---
+++ src/editor_proc.ts
@@ -0,0 +1,131 @@
+import { ipcRenderer } from 'electron';
+import { File } from './files';
+import $ from 'jquery';
+
+const tab = '    ';
+const tabLength = tab.length;
+
+export function processNewline(event: JQuery.KeyDownEvent): boolean {
+    event.preventDefault();
+    const el = document.getElementById('main-editor');
+    const sel = window.getSelection();
+
+    var range = new Range();
+    range.setStart(sel.anchorNode, 0);
+    range.setEnd(sel.anchorNode, sel.anchorOffset);
+
+    const rangeString = range.toString();
+    const rangeSplit = rangeString.split('\n');
+    const lineData = rangeSplit ? rangeSplit[rangeSplit.length-1] : rangeString;
+
+    var spaceCount = 0;
+    for (var i=0; i<lineData.length; i++) {
+        if (lineData[i] == ' ') { spaceCount++; } else break;
+    }
+
+    console.log(spaceCount);
+    const tabCount = Math.floor(spaceCount / tabLength);
+    const insertTab = tabCount > 0;
+    if (insertTab) {
+        document.execCommand('insertLineBreak');
+        for (var i=0; i<tabCount; i++) { document.execCommand('insertHTML', false, tab); }
+    } else {
+        document.execCommand('insertLineBreak');
+    }
+
+    return false;
+}
+
+export function processTab(event: JQuery.KeyDownEvent): boolean {
+    event.preventDefault();
+    const sel = window.getSelection();
+
+    // Todo: proess outdents and multiline tabbing
+    if (sel.anchorNode != sel.focusNode || sel.anchorOffset != sel.focusOffset) {
+        if (event.shiftKey) {
+            
+        } else {
+            
+        }
+    } else if (event.shiftKey) {
+        console.log('Outdent');
+
+        const anchor = sel.anchorNode as any;
+        var range = new Range();
+        range.setStart(sel.anchorNode, 0);
+        range.setEnd(sel.anchorNode, anchor.length);
+        const line = range.toString();
+
+        var spaceCount = 0;
+        for (var i=0; i<line.length; i++) {
+            if (line[i] == ' ') { spaceCount++; } else break;
+        }
+
+        const tabCount = Math.floor(spaceCount / tabLength);
+        const outdent = tabCount > 0;
+        if (outdent) {
+            const anchorNode = sel.anchorNode;
+            const lastRange = sel.getRangeAt(0);
+            sel.removeAllRanges();
+            var tabEnd = new Range();
+            tabEnd.setStart(anchorNode, spaceCount);
+            tabEnd.setEnd(anchorNode, spaceCount);
+            sel.addRange(tabEnd);
+
+            processBackspace(null);
+
+            sel.removeAllRanges();
+            sel.addRange(lastRange);
+        }
+    } else {
+        document.execCommand('insertHTML', false, tab);
+    }
+
+    return false;
+}
+
+export function processBackspace(_event: JQuery.KeyDownEvent): boolean {
+    var sel = window.getSelection();
+    var tabRange = new Range();
+
+    const endOffset = sel.anchorOffset;
+    const startOffset = endOffset > 0 ? endOffset - tabLength : endOffset;
+    tabRange.setStart(sel.anchorNode, startOffset);
+    tabRange.setEnd(sel.anchorNode, endOffset);
+
+    const deleteTab = tabRange.toString() == tab;
+    if (deleteTab) {
+        sel.removeAllRanges();
+        sel.addRange(tabRange);
+        document.execCommand('delete', false);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export function update(file: File) {
+    $('.inner', '#main-editor')[0].innerText = file.text;
+    $('#main-editor').show();
+    $('#editor-welcome').hide();
+    updatePath(file);
+}
+
+export function updatePath(file: File) {
+    $('#navigationbar').children()[0].innerText = file.path;
+    $('#navigationbar').show();
+}
+
+export function write(_file: File) {
+    const el = $('.inner', '#main-editor')[0];
+    ipcRenderer.send('editorWriteReturn', el.innerText);
+}
+
+export function close(_file: File) {
+    $('.inner', '#main-editor')[0].innerText = '';
+    $('#navigationbar').children()[0].innerText = '';
+
+    $('#main-editor').hide();
+    $('#navigationbar').hide();
+    $('#editor-welcome').show();
+}
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,12 +1,10 @@
 import { ipcRenderer, remote } from 'electron';
 import { AppEvent } from './events';
-import { File } from './files';
 import Main from './main';
+import * as EditorProcesses from './editor_proc';
 import $ from 'jquery';
 
 export var currentWindow: Electron.BrowserWindow;
-const tab = '    ';
-const tabLength = tab.length;
 
 if (!Main.windowCreated) {
     addListeners();
@@ -15,16 +13,16 @@ if (!Main.windowCreated) {
 
 const ipcMethods = {
     'windowCreated': windowCreated,
-    'editorUpdate': updateEditor,
-    'editorPathUpdate': updateEditorPath,
-    'editorWrite': editorWrite,
-    'editorClose': closeEditor
+    'editorUpdate': EditorProcesses.update,
+    'editorPathUpdate': EditorProcesses.updatePath,
+    'editorWrite': EditorProcesses.write,
+    'editorClose': EditorProcesses.close
 };
 
 const keyMethods = {
-    'Enter': processNewline,
-    'Tab': processTab,
-    'Backspace': processBackspace
+    'Enter': EditorProcesses.processNewline,
+    'Tab': EditorProcesses.processTab,
+    'Backspace': EditorProcesses.processBackspace
 };
 
 ipcRenderer.on('main', (_event, args) => {
@@ -57,102 +55,4 @@ function createTitlebar() {
 
 function windowCreated(_data: any) {
     currentWindow = remote.getCurrentWindow();
-}
-
-function processNewline(event: JQuery.KeyDownEvent): boolean {
-    event.preventDefault();
-    const el = document.getElementById('main-editor');
-    const sel = window.getSelection();
-
-    var range = new Range();
-    range.setStart(sel.anchorNode, 0);
-    range.setEnd(sel.anchorNode, sel.anchorOffset);
-
-    const rangeString = range.toString();
-    const rangeSplit = rangeString.split('\n');
-    const lineData = rangeSplit ? rangeSplit[rangeSplit.length-1] : rangeString;
-
-    var spaceCount = 0;
-    for (var i=0; i<lineData.length; i++) {
-        if (lineData[i] == ' ') { spaceCount++; } else break;
-    }
-
-    console.log(spaceCount);
-    const tabCount = Math.floor(spaceCount / tabLength);
-    const insertTab = tabCount > 0;
-    if (insertTab) {
-        document.execCommand('insertLineBreak');
-        for (var i=0; i<tabCount; i++) { document.execCommand('insertHTML', false, tab); }
-    } else {
-        document.execCommand('insertLineBreak');
-    }
-
-    return false;
-}
-
-function processTab(event: JQuery.KeyDownEvent): boolean {
-    event.preventDefault();
-    const sel = window.getSelection();
-
-    // Todo: proess outdents and multiline tabbing
-
-    if (sel.anchorNode != sel.focusNode || sel.anchorOffset != sel.focusOffset) {
-        if (event.shiftKey) {
-            
-        } else {
-            
-        }
-    } else if (event.shiftKey) {
-        
-    } else {
-        document.execCommand('insertHTML', false, tab);
-    }
-
-    return false;
-}
-
-function processBackspace(_event: JQuery.KeyDownEvent): boolean {
-    var sel = window.getSelection();
-    var tabRange = new Range();
-
-    const endOffset = sel.anchorOffset;
-    const startOffset = endOffset > 0 ? endOffset - tabLength : endOffset;
-    tabRange.setStart(sel.anchorNode, startOffset);
-    tabRange.setEnd(sel.anchorNode, endOffset);
-
-    const deleteTab = tabRange.toString() == tab;
-    if (deleteTab) {
-        sel.removeAllRanges();
-        sel.addRange(tabRange);
-        document.execCommand('delete', false);
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function updateEditor(file: File) {
-    $('.inner', '#main-editor')[0].innerText = file.text;
-    $('#main-editor').show();
-    $('#editor-welcome').hide();
-    updateEditorPath(file);
-}
-
-function updateEditorPath(file: File) {
-    $('#navigationbar').children()[0].innerText = file.path;
-    $('#navigationbar').show();
-}
-
-function editorWrite(_file: File) {
-    const el = $('.inner', '#main-editor')[0];
-    ipcRenderer.send('editorWriteReturn', el.innerText);
-}
-
-function closeEditor(_file: File) {
-    $('.inner', '#main-editor')[0].innerText = '';
-    $('#navigationbar').children()[0].innerText = '';
-
-    $('#main-editor').hide();
-    $('#navigationbar').hide();
-    $('#editor-welcome').show();
 }
